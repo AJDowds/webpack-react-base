001e6cb66d24eb4033a7821d5de1a163c3270833
Anthony Dowds <ajdowds89@gmail.com>
2021-04-29 22:31:57 +0700

    Successfully pulled in my library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React from 'react';
- 
-const App = ({ title }) =>
-  <div>{title}</div>;
- 
+import React from "react";
+import Testo from "webpack-react-base-auth";
+
+const App = ({ title }) => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Testo />
+    </div>
+  );
+};
+
 export default App;
