e5db8c5912a9e8afb85c78e45b0638a1fdadb217
AJDowds <ajdowds89@gmail.com>
2022-02-28 22:22:48 +0000

    fixed title

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 
-const title = 'React with Webpack and Babelasd';
+const title = 'Webpack React Base';
  
 ReactDOM.render(
     <App title={title} />,
