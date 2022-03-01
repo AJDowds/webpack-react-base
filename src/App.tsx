5ead5b258518a1c03ec8773636d54d966afbc6ef
AJDowds <ajdowds89@gmail.com>
2022-03-01 21:40:06 +0000

    Corrected Testo import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import Testo from "webpack-react-base-auth";
+import { Testo } from "webpack-react-base-auth";
 
 interface AppInterface {
   title: string;
@@ -9,7 +9,7 @@ const App: React.FC<AppInterface> = ({ title }) => {
   return (
     <div>
       <h1>Hello + {title}</h1>
-      <Testo />
+      <Testo title="Test" />
     </div>
   );
 };
